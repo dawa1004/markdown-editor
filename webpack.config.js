b1a75785453e75d4bf3b7ca919ecd889fb7ce4b9
dawa1004 <wr1004wr1004wr@gmail.com>
2020-09-19 21:25:44 +0900

    fin commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
 const path = require('path')
 
 module.exports = {
-  entry:'./src/index.tsx', // 最初に読み込むファイル .tsx→TypeScript + JSX を使用しているファイル
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
-        // .tsx で終わるファイルに対して、ts-loader を実行する
-        test: /\.tsx$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/, // 除外するファイル
+        exclude: /node_modules/,
       },
     ],
   },
-  resolve: { // モジュールとして解決するファイルの拡張子を指定→外部ファイルやライブラリ（node_modules 以下のファイル）を使うファイルの拡張子
-    extensions: ['.js', '.ts', 'tsx'],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
   },
-  output: { // 出力するファイルの設定
-    // webpack.config.js の置いてあるディレクトリにある dist というディレクトリに対して、ファイル名 index.js で出力
-    path:path.resolve(__dirname,'dist'),
-    filename:'index.js',
-    publicPath:'dist/',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: 'dist/',
   },
   devServer: {
-    hot: true, // ファイルを変更すると自動的にブラウザに反映させるフラグ
-    open: true, //起動時にブラウザで開くフラグ
+    hot: true,
+    open: true,
   }
 }
